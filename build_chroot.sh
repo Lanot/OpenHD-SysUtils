e5dd19bb6c3986dc09f299874c8cefeacb83519c
tolik <lanot.biz@gmail.com>
2026-04-16 21:23:46 +0300

    reverted `build_chroot.sh` and added `build_lite.sh`

--- build_chroot.sh
+++ build_chroot.sh
@@ -3,13 +3,11 @@ set -euo pipefail
 
 chmod 1777 /tmp || true
 
-sudo apt-get update --fix-missing
-sudo apt-get install -y cmake g++ python3
+apt-get update --fix-missing
+apt-get install -y cmake g++ python3
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
 cd build
 cpack -G DEB
-
-sudo mkdir -p /out && sudo chmod 777 /out
 cp *.deb /out/
